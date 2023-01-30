150537ed5a2f2855859cf8f8ea17771c70720318
rafaelduartejv <rafaelduarte.jv@gmail.com>
2023-01-30 16:48:34 -0300

    Create Task, Get All Tasks and Get Task By ID

--- src/main/java/com/rafasantos/todolist/repository/TaskRepository.java
+++ src/main/java/com/rafasantos/todolist/repository/TaskRepository.java
@@ -1,4 +1,10 @@
 package com.rafasantos.todolist.repository;
 
-public interface TaskRepository {
+
+import com.rafasantos.todolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
